613be7a6b193c396af64ab1fe740784e125893d2
Evakulenko <evgenijyuri4@gmail.com>
2015-08-14 23:01:12 +0300

    add one folder

---
+++ java/Main.java
@@ -0,0 +1,10 @@
+package java;
+
+/**
+ * Created by Евгений on 14.08.2015.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hi, it's time to eat some watermelon!");
+    }
+}
