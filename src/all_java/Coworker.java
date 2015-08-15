5f61e8f18daea2ce7e3912c8d7fa42428421211b
Evakulenko <evgenijyuri4@gmail.com>
2015-08-15 11:06:21 +0300

    my second commit

---
+++ src/all_java/Coworker.java
@@ -0,0 +1,15 @@
+package all_java;
+
+/**
+ * Created by Евгений on 15.08.2015.
+ */
+public class Coworker {
+
+    String workplace = "Resurs Pridneprovya";
+    int salary = 3500;
+
+    public void birthday (){
+        System.out.println("I have a BirthDay!!!");
+    }
+
+}
--- src/all_java/Main.java
+++ src/all_java/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hmm.... Git is not so easy");
+        Salesman Shota = new Salesman();
+        Shota.get_salary();
     }
 
 }
